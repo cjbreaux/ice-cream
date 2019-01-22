05ea8066441186ab28059a76608d67e449c7c42b
Chris Breaux and Matt Groberg <chris+matt@>
2019-01-22 13:19:50 -0800

    add text and images as list items using forEach loops

---
+++ js/scripts.js
@@ -0,0 +1,12 @@
+$(document).ready(function(){
+  var chrisIces = ["chocolate", "vanilla", "pumpkin"]
+  var mattIces = ["mint","cookie-dough","chocolate"]
+
+  chrisIces.forEach(function(chrisIce){
+    $("#chrisList").append("<li>" + chrisIce + "</li>")
+  })
+  mattIces.forEach(function(iceCream){
+    console.log("<img src='img/" + iceCream +".jpeg'");
+    $("#mattList").append("<img src='img/" + iceCream +".jpeg'>")
+  })
+})
