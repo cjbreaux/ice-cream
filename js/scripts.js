92c0be5d60fb58b6e14feb77b45cb8333cf41b43
Chris Breaux and Matt Groberg <chris@>
2019-01-22 13:28:43 -0800

    practice looping with forEach

--- js/scripts.js
+++ js/scripts.js
@@ -4,9 +4,13 @@ $(document).ready(function(){
 
   chrisIces.forEach(function(chrisIce){
     $("#chrisList").append("<li>" + chrisIce + "</li>")
-  })
+  });
   mattIces.forEach(function(iceCream){
     console.log("<img src='img/" + iceCream +".jpeg'");
     $("#mattList").append("<img src='img/" + iceCream +".jpeg'>")
-  })
-})
+  });
+  // test selectors later
+  $("p").click(function(){
+    $("#chrisList").toggle();
+  });
+});
